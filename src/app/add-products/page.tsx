a4b3ea11b65ae08a5612251a5829407671ffacbd
Seth Ofoma <ofomacedric.ceo@gmail.com>
2024-10-20 12:41:02 +0100

    just changed and deleted  my spread ...authpage

--- src/app/add-products/page.tsx
+++ src/app/add-products/page.tsx
@@ -2,7 +2,7 @@ import prisma from "@/lib/db/prisma";
 import { redirect } from "next/navigation";
 import FromSubmitButton from "../../components/FormSubmitButton";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import { authOptions } from "../api/auth/route";
 
 export const metadata = {
   title: "Add Product - Horimazo",
