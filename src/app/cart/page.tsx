9eeb918a139c203e8105a635a6ab4a0c8251a7bd
Seth Ofoma <ofomacedric.ceo@gmail.com>
2024-10-21 20:36:51 +0100

    added a new package

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -2,6 +2,7 @@ import { getCart } from "@/lib/db/cart";
 import { formatPrice } from "@/lib/format";
 import CartEntry from "./CartEntry";
 import { setProductQuantity } from "./actions";
+import Link from "next/link";
 
 export const metadata = {
   title: "Your Cart - Horimazo",
@@ -11,7 +12,7 @@ export default async function CartPage() {
   const cart = await getCart();
 
   return (
-    <div>
+     <div>
       <h1 className="mb-6 text-3xl font-bold">Shopping Cart</h1>
       {cart?.items.map((cartItem) => (
         <CartEntry
@@ -20,14 +21,27 @@ export default async function CartPage() {
           setProductQuantity={setProductQuantity}
         />
       ))}
-      {!cart?.items.length && <p>Your cart is empty.</p>}
-      <div className="flex flex-col items-end sm:items-center">
-        <p className="mb-3 font-bold">
-          Total: {formatPrice(cart?.subtotal || 0)}
-        </p>
-        <button className="btn-primary btn sm:w-[200px]">Checkout</button>
-      </div>
-      
+
+      {!cart?.items.length ? (
+        <div className="flex flex-col items-end sm:items-center">
+          <p className="text-amber-700 font-semibold mb-5">Your cart is empty.</p>
+          <Link href="http://localhost:3000/">
+            <p className="btn-secondary btn sm:w-[200px]">Go Shopping</p>
+          </Link>
+        </div>
+      ) : (
+        <div className="flex flex-col items-end sm:items-center">
+          <p className="mb-3 font-bold">
+            Total: {formatPrice(cart?.subtotal || 0)}
+          </p>
+          <button
+            className="btn-primary btn sm:w-[200px]"
+            disabled={!cart?.items.length}
+          >
+            Checkout
+          </button>
+        </div>
+      )}
     </div>
   );
 }
